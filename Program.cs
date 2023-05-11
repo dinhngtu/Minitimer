d26b44782d8ed364bc3fec98043c7510b35262b1
Tu Dinh <1257909+dinhngtu@users.noreply.github.com>
2023-05-11 02:14:25 +0200

    check startup mutex

--- Program.cs
+++ Program.cs
@@ -1,7 +1,8 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,14 +13,19 @@ namespace Minitimer {
         /// </summary>
         [STAThread]
         static void Main() {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            var f = new Form1();
-            var currentScreen = Screen.FromControl(f);
-            var realX = currentScreen.WorkingArea.Width - f.Width - f.LogicalToDeviceUnits(8);
-            var realY = currentScreen.WorkingArea.Height - f.Height - f.LogicalToDeviceUnits(8);
-            f.Location = new Point(realX, realY);
-            Application.Run(f);
+            using (var m = new Mutex(true, "{5F73BBBC-645D-4209-8EB7-A33A1C553B17}", out var created)) {
+                if (!created) {
+                    return;
+                }
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                var f = new Form1();
+                var currentScreen = Screen.FromControl(f);
+                var realX = currentScreen.WorkingArea.Width - f.Width - f.LogicalToDeviceUnits(8);
+                var realY = currentScreen.WorkingArea.Height - f.Height - f.LogicalToDeviceUnits(8);
+                f.Location = new Point(realX, realY);
+                Application.Run(f);
+            }
         }
     }
 }
